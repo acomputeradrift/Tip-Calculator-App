aaabece3615e9fd7eaaf37b75d827056fff145df
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-10 11:03:56 -0700

    Initial Commit

---
+++ Tip Calculator App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator App
+//
+//  Created by Jamie on 2018-08-10.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
